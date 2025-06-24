2112c36426e0c580ec1ece1a1f846c65428f2fe1
Mikita Buinavets <mikita.buinavets@arua.co>
2025-06-24 22:18:12 +0300

    Implement ArrayList and LinkedList creation methods, and enhance List sorting with custom comparator

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -6,15 +6,15 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
-    List<String> res = ArrayList<>();
-    for (int i = 0; i < sourceList.size(); i++) {
-        int listEl = sourceList.get(i);
-        if (listEl % 2 == 0) {
-            sourceList.add(0, listEl);
-        } else {
-            sourceList.add(listEl);
+        LinkedList<String> res = ArrayList<>();
+        for (int i = 0; i < sourceList.size(); i++) {
+            int listEl = sourceList.get(i);
+            if (listEl % 2 == 0) {
+                sourceList.add(0, listEl);
+            } else {
+                sourceList.add(listEl);
+            }
         }
-    }
-    return res;
+        return res;
     }
 }
