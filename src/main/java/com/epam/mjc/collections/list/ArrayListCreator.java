f8079477692076bf228e973527f1759079d244aa
Mikita Buinavets <mikita.buinavets@arua.co>
2025-06-24 22:30:48 +0300

    Fix ArrayListCreator logic to correctly add elements from sourceList based on updated index condition

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
         ArrayList<String> res = new ArrayList<>();
-        for (int i = 1; i < sourceList.size(); i++) {
-            if (i % 3 == 0) {
+        for (int i = 0; i < sourceList.size(); i++) {
+            if ((i + 1) % 3 == 0) {
                 for (int j = 0; j < 2; j++) {
                     res.add(sourceList.get(i));
                 }
